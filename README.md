8d5f3aa99556e9bdfd6b5b2f98c21382a85351f5
Jake Soenneker <jake@soenneker.com>
2026-01-06 18:35:49 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Initializing...
+﻿[![](https://img.shields.io/nuget/v/soenneker.dictionaries.singletons.svg?style=for-the-badge)](https://www.nuget.org/packages/soenneker.dictionaries.singletons/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.dictionaries.singletons/publish-package.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.dictionaries.singletons/actions/workflows/publish-package.yml)
+[![](https://img.shields.io/nuget/dt/soenneker.dictionaries.singletons.svg?style=for-the-badge)](https://www.nuget.org/packages/soenneker.dictionaries.singletons/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Dictionaries.Singletons
+### An externally initializing singleton dictionary that uses double-check asynchronous locking, with optional async and sync disposal
+
+## Installation
+
+```
+dotnet add package Soenneker.Dictionaries.Singletons
+```
---
+++ test/Soenneker.Dictionaries.Singletons.Tests/SingletonDictionaryTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AwesomeAssertions;
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Dictionaries.Singletons.Tests;
+
+public sealed class SingletonDictionaryTests : UnitTest
+{
+    [Fact]
+    public async Task Keyed_initializes_once()
+    {
+        CancellationToken cancellationToken = TestContext.Current.CancellationToken;
+        int calls = 0;
+
+        var dict = new SingletonDictionary<string>(key =>
+        {
+            Interlocked.Increment(ref calls);
+            return new ValueTask<string>($"v-{key}");
+        });
+
+        string a = await dict.Get("k1", cancellationToken);
+        string b = await dict.Get("k1", cancellationToken);
+
+        a.Should().Be("v-k1");
+        b.Should().Be("v-k1");
+        calls.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task T1_argFactory_only_runs_when_missing()
+    {
+        CancellationToken cancellationToken = TestContext.Current.CancellationToken;
+        int argFactoryCalls = 0;
+
+        var dict = new SingletonDictionary<string, int>((key, arg) =>
+            new ValueTask<string>($"{key}-{arg}"));
+
+        string first = await dict.Get("k", () =>
+        {
+            Interlocked.Increment(ref argFactoryCalls);
+            return 123;
+        }, cancellationToken);
+
+        string second = await dict.Get("k", () =>
+        {
+            Interlocked.Increment(ref argFactoryCalls);
+            return 999;
+        }, cancellationToken);
+
+        first.Should().Be("k-123");
+        second.Should().Be("k-123");
+        argFactoryCalls.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task T1T2_tuple_argFactory_only_runs_when_missing()
+    {
+        CancellationToken cancellationToken = TestContext.Current.CancellationToken;
+        int argFactoryCalls = 0;
+
+        var dict = new SingletonDictionary<string, int, int>((key, a1, a2) =>
+            new ValueTask<string>($"{key}-{a1}-{a2}"));
+
+        string first = await dict.Get("k", () =>
+        {
+            Interlocked.Increment(ref argFactoryCalls);
+            return (1, 2);
+        }, cancellationToken);
+
+        string second = await dict.Get("k", () =>
+        {
+            Interlocked.Increment(ref argFactoryCalls);
+            return (9, 9);
+        }, cancellationToken);
+
+        first.Should().Be("k-1-2");
+        second.Should().Be("k-1-2");
+        argFactoryCalls.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task TryGet_and_GetAll_work()
+    {
+        CancellationToken cancellationToken = TestContext.Current.CancellationToken;
+        var dict = new SingletonDictionary<string>(key => new ValueTask<string>($"v-{key}"));
+
+        dict.TryGet("k", out _).Should().BeFalse();
+
+        _ = await dict.Get("k", cancellationToken);
+
+        dict.TryGet("k", out string? value).Should().BeTrue();
+        value.Should().Be("v-k");
+
+        var all = await dict.GetAll(cancellationToken);
+        all.Should().HaveCount(1);
+        all.Should().ContainKey("k").WhoseValue.Should().Be("v-k");
+    }
+
+    [Fact]
+    public async Task Clear_disposes_values()
+    {
+        CancellationToken cancellationToken = TestContext.Current.CancellationToken;
+        int disposed = 0;
+
+        var dict = new SingletonDictionary<DisposableValue>(key =>
+            new ValueTask<DisposableValue>(new DisposableValue(() => Interlocked.Increment(ref disposed))));
+
+        _ = await dict.Get("a", cancellationToken);
+        _ = await dict.Get("b", cancellationToken);
+
+        await dict.Clear(cancellationToken);
+
+        disposed.Should().Be(2);
+        (await dict.GetKeys(cancellationToken)).Should().BeEmpty();
+    }
+
+    private sealed class DisposableValue : IDisposable
+    {
+        private readonly Action _onDispose;
+
+        public DisposableValue(Action onDispose)
+        {
+            _onDispose = onDispose;
+        }
+
+        public void Dispose()
+        {
+            _onDispose();
+        }
+    }
+}
